3817adc77212dd56bc82db0b3ba626b1b8ac9105
kakudo415 <31888089+kakudo415@users.noreply.github.com>
2023-06-07 20:25:28 +0900

    feat: Add syntax highlighting

--- app/[year]/[month]/[filename]/Content.tsx
+++ app/[year]/[month]/[filename]/Content.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {vscDarkPlus} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 import styles from './Content.module.css'
 
@@ -7,9 +12,27 @@ export function Content({ className, property, content }: { className: string, p
   return (
     <main className={`${className} ${styles.content}`}>
       <h1>{ property.title }</h1>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>
-        { content }
-      </ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}
+        children={content}
+        components={{
+          code({node, inline, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                style={vscDarkPlus}
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      />
     </main>
   )
 }
