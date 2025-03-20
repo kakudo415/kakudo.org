717a06ec0ca72cf92247dcdb90d5c935fbb7169c
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2025-03-21 02:36:03 +0900

    chore: 初期設定ファイルの追加

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+}
+
+const withMDX = createMDX({})
+
+export default withMDX(nextConfig)
